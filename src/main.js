29a091234db140e7e3fd9c7fda1346192c443c72
bonial_virto <svetlana.suslova2031@gmail.com>
2020-11-03 19:40:03 +0100

    added BaseButton component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App';
 import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
