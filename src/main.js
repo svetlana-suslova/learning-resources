2cfadfd124887a25fc0f40decd0a422ac757f0d9
bonial_virto <svetlana.suslova2031@gmail.com>
2020-11-03 19:15:47 +0100

    added BaseCard component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue';
-import App from './App';
 
-createApp(App).mount('#app');
+import App from './App';
+import BaseCard from './components/ui/BaseCard';
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+
+app.mount('#app');
