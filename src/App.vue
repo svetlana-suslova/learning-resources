e9740ea80571efe9fd973d7d0fbbb57a63912247
bonial_virto <svetlana.suslova2031@gmail.com>
2020-11-03 17:49:42 +0100

    setup and first steps

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+    export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.com'
+                }
+            ]
+        };
+    },
+    };
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: "Jost", sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
