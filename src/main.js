c6c75ebed82354e02b4871f4a945870f36bd65dd
bonial_virto <svetlana.suslova2031@gmail.com>
2020-11-05 16:35:41 +0100

    implemented custom Base Dialog component to warn about input errors

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from 'vue';
 import App from './App';
 import BaseCard from './components/ui/BaseCard';
 import BaseButton from './components/ui/BaseButton';
+import BaseDialog from './components/ui/BaseDialog';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
